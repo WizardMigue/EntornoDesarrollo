818dd7094627dfdef0c56d255679d2c7bb9d1a3f
DAM1ALU15 <DAM1ALU15@B01-PC15.IESGUARA.LOCAL>
2022-04-04 11:51:53 +0200

    Clase Persona

---
+++ Prog01/src/Persona.java
@@ -0,0 +1,69 @@
+
+/**
+ * 
+ * Clase Persona es para crear objetos que hacen referencia a personas
+ * Nuestras personas sólo tienen una edad y un nombre
+ * 
+ * @author Miguel Ovejero
+ * @version 1.0
+ *
+ */
+
+public class Persona {
+	private String nombre;
+	private int edad;
+
+	/**
+	 * Metodo constructor con los parámetros edad (entero) y nombre (String).
+	 * @param nombre sin restricción.
+	 * @param edad es un entero y de momento no controlamos nada,
+	 */
+
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param devuelve el nombre (String) del objeto dentro de la clase persona.
+	 */
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return devuelve el nombre pasado como texto.
+	 */
+
+	public int getEdad() {
+		return edad;
+	}
+
+	/**
+	 * Establece la edad pasada como entero.
+	 * @param edad es un entero sin restricciones.
+	 */
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	/**
+	 * Metodo que nos permite cambiar la edad (int)
+	 * 
+	 */
+	
+	
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
+	}
+
+}
